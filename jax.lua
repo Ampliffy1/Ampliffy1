aa6cc28fa6fd877814bd53d2921006fe2dbaf75e
Ampliffy1 <148794147+Ampliffy1@users.noreply.github.com>
2023-10-23 16:09:49 +0000

    Create jax.lua

---
+++ jax.lua
@@ -0,0 +1,13 @@
+function highlightPlayers(esp)
+    local players = game:GetService("Players")
+    for i, player in ipairs(players:GetPlayers()) do
+      local humanoidRootPart = player.Character.HumanoidRootPart
+      humanoidRootPart.BrickColor = esp and BrickColor.Red or BrickColor.White
+    end
+  end
+  highlightPlayers(true)
+  game:GetService("UserInputService").InputBegan:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.Q then
+      highlightPlayers(not highlightPlayers())
+    end
+  end)
